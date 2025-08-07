531cd894c8ead7890b6a02d83d8cc17768ced028
greg-beach <gbeach22@gmail.com>
2025-08-07 15:00:22 -0400

    Added method for authorized users to delete their own chirps. Verifies that user is authenticated

---
+++ handler_chirps_delete.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/greg-beach/chirpy/internal/auth"
+)
+
+func (cfg *apiConfig) handlerChirpsDelete(w http.ResponseWriter, r *http.Request) {
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not find JWT", err)
+		return
+	}
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not validate JWT", err)
+		return
+	}
+
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Could not retrieve Chirp", err)
+		return
+	}
+
+	if userID != dbChirp.UserID {
+		respondWithError(w, http.StatusForbidden, "Not the author of the Chirp", err)
+		return
+	}
+
+	err = cfg.db.DeleteChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not delete chirp", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
